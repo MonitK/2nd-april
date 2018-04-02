6be9dab0135795612e41aee321db37bd76ce0c0c
MonitK <mkapoor@ddn.upes.ac.in>
2018-04-02 15:21:11 +0530

    first commit on 2nd april,before branching

---
+++ boolean.cpp
@@ -0,0 +1,39 @@
+#include<iostreatm
+<<<<<<< HEAD
+//
+//
+//
+using namespace std;
+//main begins here
+int main()
+
+
+
+//using namespace std;
+ //int main()
+>>>>>>> test
+>>>>>>> testing
+{
+int x;
+x=20;
+bool b=true;
+if(x<10)
+{
+b=false;
+}
+if(b==false)
+{
+cout<<"wrong Value"<<endl;
+}
+else
+{
+cout<<"true value"<<endl;
+}
+cout<<sizeof(b);
+}
+------------------------------------------------------------------
+[user1@clab monit]$ g++ boolean.cpp -Wno-deprecated -o boolean
+[user1@clab monit]$ ./boolean
+true value
+1
+
